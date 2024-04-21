28004a49c4bb76c45c3f492407a19faf72b51168
Bogdan Petrut <bogdanpetrut.319@gmail.com>
2024-04-21 16:41:31 +0200

    Refactor game picker functionality and add new game options

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const games = document.querySelectorAll('.game');
 
+let selected_game = null;
+
 games.forEach(game => {
-    game.addEventListener('click', () => {
-        console.log(game.getAttributeNode('data-game').value);
-    });
+    let game_name = game.getAttributeNode('data-game').value;
+    game.addEventListener('click', () => selectGame(game_name));
+    
 });
+function selectGame(game_name) {
+    console.log(game_name);
+    switch(game_name) {
+        case 'brankosaurus':
+            window.location.href = 'brankosaurus/index.html';
+            break;
+        case 'pikachu':
+            window.location.href = 'pikachu/index.html';
+            break;
+    }
+}
